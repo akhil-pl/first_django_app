4513f14fe17ae627d4571c959796f298b0820f1f
Akhil P L <akhilplx@gmail.com>
2023-06-23 12:05:16 +0530

    Finish

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # first_django_app
  Steping to django web development
+This is a very basic django web app to get the multiplication table of an input integer upto another integer input
+
+## Local setup
+You can run this app locallly
+After installing python and django, run the code 'python manage.py runserver'
--- first_django_app/view_functions.py
+++ first_django_app/view_functions.py
@@ -2,6 +2,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.template.defaulttags import register
+
+
+@register.filter
+def get_range(value):
+    return list(range(1, int(value)+1))
+
+
 def home(request):
     return render(request, "index.html", {
         'first':10,
@@ -10,8 +18,8 @@ def home(request):
     # return HttpResponse("Hello World !!")
 
 def generate(request):
-    num = request.GET.get("num")
-    upto = request.GET.get("upto")
+    num = request.POST.get("num")
+    upto = request.POST.get("upto")
     return render(request, "table.html", {
         'num':num,
         'upto':upto
