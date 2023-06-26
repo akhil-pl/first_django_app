b93ff7ac65ee868b5954a2aa971be7c45fae0dc5
Akhil P L <akhilplx@gmail.com>
2023-06-27 01:21:37 +0530

    register form

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('test/', views.test_view, name="test-url"),
-    path('register/', views.register_user, name="register")
+    path('register/', views.register_user, name="register"),
+    path('accounts/', include("django.contrib.auth.urls")), #the html will be checked on "templates/registration folder"
 ]
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from .forms import Register
 
 # Create your views here.
@@ -9,6 +10,17 @@ def  test_view(request):
 def register_user(request):
     if request.method == "POST":
         form = Register(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                form.cleaned_data.get("username"),
+                form.cleaned_data.get("email"),
+                form.cleaned_data.get("password"),
+            )
+            user.first_name = form.cleaned_data.get("first_name")
+            user.last_name = form.cleaned_data.get("last_name")
+            user.save()
+            return HttpResponse('Thanks')
+        
     else:
         form = Register
 
